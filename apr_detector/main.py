0d813371232a58b5aa2e9990adf1763ae40b1884
guayacamole <guy.xd2000@gmail.com>
2024-06-30 23:06:38 -0600

    almost

--- apr_detector/main.py
+++ apr_detector/main.py
@@ -1,5 +1,4 @@
 import os
-import time
 import re
 
 
@@ -8,19 +7,7 @@ def get_machine_ip():
   ip_address_match = re.search(r'inet ([\d.]+)/', result)
   print(f"Machine IP: {ip_address_match.group(
       1) if ip_address_match else 'Not Found'}")
-  if ip_address_match:
-    return ip_address_match.group(1)
-  return None
-
-
-def get_default_gateway():
-  result = os.popen('ip route').read()
-  gateway_match = re.search(r'default via ([\d.]+)', result)
-  print(f"Default Gateway: {gateway_match.group(
-      1) if gateway_match else 'Not Found'}")
-  if gateway_match:
-    return gateway_match.group(1)
-  return None
+  return ip_address_match.group(1) if ip_address_match else None
 
 
 def get_arp_table():
@@ -32,32 +19,24 @@ def get_arp_table():
     parts = line.split()
     if len(parts) >= 4 and re.match(r'[\d.]+', parts[1].strip('()')):
       arp_table.append({'ip': parts[1].strip('()'), 'mac': parts[3]})
-  print(f"ARP Table: {arp_table}")
   return arp_table
 
 
-def detect_arp_changes():
+def check_host_arp():
   machine_ip = get_machine_ip()
-  print(f"Machine IP: {machine_ip}")
-  initial_arp_table = [
-      entry for entry in get_arp_table() if entry['ip'] == machine_ip]
-  print("Initial ARP table:")
-  for entry in initial_arp_table:
-    print(f"IP={entry['ip']}, MAC={entry['mac']}")
-  print('debug ..')
+  if not machine_ip:
+    print("Machine IP not found. Exiting.")
+    return
 
-  while True:
-    time.sleep(5)
-    current_arp_table = [
-        entry for entry in get_arp_table() if entry['ip'] == machine_ip]
+  arp_table = get_arp_table()
+  host_arp_entry = next(
+      (entry for entry in arp_table if entry['ip'] == machine_ip), None)
 
-    for entry in current_arp_table:
-      if entry not in initial_arp_table:
-        print(f"Change detected for this machine: IP={
-              entry['ip']}, MAC={entry['mac']}")
-        initial_arp_table = current_arp_table
-
-    initial_arp_table = current_arp_table
+  if host_arp_entry:
+    print(f"Host ARP entry: IP={host_arp_entry['ip']}, MAC={
+          host_arp_entry['mac']}")
+  else:
+    print("No ARP entry found for the host machine.")
 
 
-detect_arp_changes()
+check_host_arp()
